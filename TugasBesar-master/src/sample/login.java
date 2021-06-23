69e9da66245574c3e7d40482ebec669e159f1f63
zidananggoro <52589583+zidananggoro@users.noreply.github.com>
2021-06-23 14:21:13 +0700

    Add files via upload

---
+++ TugasBesar-master/src/sample/login.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class login {
+    @FXML
+    private TextField fieldEmail;
+
+    @FXML
+    private PasswordField fieldPassword;
+
+    @FXML
+    private Button btnlogin;
+
+}
