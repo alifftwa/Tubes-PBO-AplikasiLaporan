0f3c82ad3cfb7eaa46c62fadd7069f6296f32830
zidananggoro <52589583+zidananggoro@users.noreply.github.com>
2021-06-29 21:33:55 +0700

    Update login.java

--- TugasBesar-master/src/sample/login.java
+++ TugasBesar-master/src/sample/login.java
@@ -1,9 +1,13 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class login {
     @FXML
@@ -14,5 +18,11 @@ public class login {
 
     @FXML
     private Button btnlogin;
+
+    public void Masuklogin() throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Stage window = (Stage) btnlogin.getScene().getWindow();
+        window.setScene(new Scene(root, 786, 550));
+    }
 
 }
